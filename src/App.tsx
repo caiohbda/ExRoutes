401dc21d003ad8defce5bb67cd7b82898d989080
Caio Henrique <caiohbda777@gmail.com>
2024-05-16 21:10:06 -0300

    React Routes

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import HomeBody from "./Routes/Home/HomeBody";
+import Promotion from "./Routes/Home/Promotion";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+
+          <Route index element={<HomeBody />} />
+          <Route path="promotion" element={<Promotion />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
